13155e32f38f604bb8baf76865871bcc5a49a57c
Alex Varju <alex@varju.ca>
2013-09-23 18:42:25 -0700

    add missing methods

--- mongoose.d.ts
+++ mongoose.d.ts
@@ -1,5 +1,93 @@
 ///<reference path='d.ts/DefinitelyTyped/node/node.d.ts' />
 
+interface ConnectionOptions {
+  db?: any;
+  server?: any;
+  replset?: any;
+  user?: string;
+  pass?: string;
+  auth?: any;
+  mongos?: boolean;
+}
+
+interface SchemaOptions {
+  autoIndex?: boolean;
+  bufferCommands?: boolean;
+  capped?: any;
+  collection?: string;
+  id?: boolean;
+  _id?: boolean;
+  minimize?: boolean;
+  read?: any;
+  safe?: boolean;
+  shardKey?: Object;
+  strict?: boolean;
+  toJSON?: Object;
+  toObject?: Object;
+  versionKey?: boolean;
+}
+
+interface SchemaTypes {
+  String: any;
+  Number: any;
+  Boolean: any;
+  Bool: any;
+  Array: any;
+  Buffer: any;
+  Date: any;
+  ObjectId: any;
+  Oid: any;
+  Mixed: any;
+}
+
+interface MongooseTypes {
+  Array: any;
+  Buffer: any;
+  Document: any;
+  Embedded: any;
+  DocumentArray: any;
+  ObjectId: any;
+}
+
+interface QueryOptions {
+  tailable?: any;
+  sort?: any;
+  limit?: any;
+  skip?: any;
+  maxscan?: any;
+  batchSize?: any;
+  comment?: any;
+  snapshot?: any;
+  hint?: any;
+  slaveOk?: any;
+  lean?: any;
+
+  /* mongo cursor options */
+  explain?: any;
+  timeout?: any;
+  awaitdata?: any;
+  raw?: any;
+  read?: any;
+  returnKey?: any;
+  maxScan?: any;
+  min?: any;
+  max?: any;
+  showDiskLoc?: any;
+  numberOfRetries?: any;
+  dbName?: any;
+  tailableRetryInterval?: any;
+  exhaust?: any;
+  partial?: any;
+}
+
+interface QueryStream extends ReadableStream {
+  readable: boolean;
+  paused: boolean;
+  pause(): void;
+  resume(): void;
+  destroy(err?: any): void;
+}
+
 declare module "mongoose" {
 
   class Mongoose {
@@ -39,8 +127,8 @@ declare module "mongoose" {
   class Connection implements EventEmitter {
     constructor(base: Mongoose);
 
-    open(connection_string: string, database?: string, port?: number, options?: any, callback?: (err: any, db?: any) => void): Connection;
-    openSet(uris: string, database?: string, options?: any, callback?: (err: any, db?: any) => void): Connection;
+    open(connection_string: string, database?: string, port?: number, options?: ConnectionOptions, callback?: (err: any, db?: any) => void): Connection;
+    openSet(uris: string, database?: string, options?: ConnectionOptions, callback?: (err: any, db?: any) => void): Connection;
     close(callback?: Function): Connection;
     collection(name: string, options?: any): Collection;
     model(name: string, schema?: Schema, collection?: string): Model<any>;
@@ -63,38 +151,168 @@ declare module "mongoose" {
   }
 
   class Schema {
-    constructor(definition: any, options?: any);
+    constructor(definition: any, options?: SchemaOptions);
 
-    static Types: {
-      ObjectId: any;
-      Mixed: any;
-    };
+    static Types: SchemaTypes;
 
     methods: any;
     statics: any;
 
-    path(path: string): any;
-    virtual(path: string): any;
+    add(obj: Object, prefix?: string): void;
+    path(path: string): SchemaType;
+    path(path: string, obj: Object): Schema;
+    eachPath(callback: Function): Schema;
+    requiredPaths(): string[];
+    pathType(path: string): string;
     pre(method: string, callback: Function): Schema;
+    post(method: string, callback: Function): Schema;
+    plugin(callback: (schema: Schema, options?: Object) => void, opts?: Object): Schema;
+    method(name: string, fn: Function): Schema;
+    method(name: Object, fn: Function): Schema;
+    static(name: string, fn: Function): Schema;
+    index(fields: Object, options?: Object): Schema;
+    set(key: string): Object;
+    set(key: string, value: Object): Schema;
+    get(key: string): Object;
+    indexTypes: string[];
+    indexes(): any[];
+    virtual(name: string, options?: Object): VirtualType;
+    virtualpath(name: string): VirtualType;
   }
 
   class SchemaType {
+    default(val: any): any;
+    index(options?: Object): SchemaType;
+    unique(bool: boolean): SchemaType;
+    sparse(bool: boolean): SchemaType;
+    set(fn: (val: any, schemaType?: SchemaType) => any): SchemaType;
+    get(fn: (val: any, schemaType?: SchemaType) => any): SchemaType;
+    validate(obj: RegExp, error: string): SchemaType;
+    validate(obj: (val: any, callback?: (val: boolean) => void) => void, error: string): SchemaType;
+    validate(obj: Object, error: string): SchemaType;
+    required(bool: boolean): SchemaType;
+    select(val: boolean): SchemaType;
   }
 
   class VirtualType {
+    get(fn: () => any): VirtualType;
+    set(fn: (val: any) => void): VirtualType;
+    applyGetters(value: any, scope: any): any;
+    applySetters(value: any, scope: any): any;
   }
 
   class Query<T extends Document> {
-    exec(): Promise;
-    exec(operation: string): Promise;
+    setOptions(options: QueryOptions): Query<T>;
+    exec(operation?: string, callback?: (err: any, res: T[]) => void): Promise;
     exec(callback: (err: any, res: T[]) => any): Promise;
-    exec(operation: string, callback: (err: any, res: T[]) => void): Promise;
+    find(criteria: Object, callback?: Function): Query<T>;
+    find(callback: Function): Query<T>;
+    cast(model: Model<Document>, obj?: Object): any;
+    $where(js: string): Query<T>;
+    $where(js: Function): Query<T>;
+    where(path?: string, val?: Object): Query<T>;
+    equals(val: any): Query<T>;
+    or(array: Object[]): Query<T>;
+    nor(array: Object[]): Query<T>;
+    and(array: Object[]): Query<T>;
+    gt(path: string, val: number): Query<T>;
+    gt(val: number): Query<T>;
+    gte(path: string, val: number): Query<T>;
+    gte(val: number): Query<T>;
+    lt(path: string, val: number): Query<T>;
+    lt(val: number): Query<T>;
+    lte(path: string, val: number): Query<T>;
+    lte(val: number): Query<T>;
+    ne(path: string, val: any): Query<T>;
+    ne(val: any): Query<T>;
+    in(path: string, val: any): Query<T>;
+    in(val: any): Query<T>;
+    nin(path: string, val: any): Query<T>;
+    nin(val: any): Query<T>;
+    all(path: string, val: any): Query<T>;
+    all(val: any): Query<T>;
+    size(path: string, val: number): Query<T>;
+    size(val: number): Query<T>;
+    regex(path: string, val: any): Query<T>;
+    regex(val: any): Query<T>;
+    maxDistance(path: string, val: number): Query<T>;
+    maxDistance(val: number): Query<T>;
+    near(path: string, ...val: number[]): Query<T>;
+    near(val: number): Query<T>;
+    nearSphere(path: string, ...val: number[]): Query<T>;
+    nearSphere(val: number): Query<T>;
+    mod(path: string, val: number[]): Query<T>;
+    mod(path: string, ...val: number[]): Query<T>;
+    mod(val: number[]): Query<T>;
+    mod(...val: number[]): Query<T>;
+    exists(path: string, val?: boolean): Query<T>;
+    exists(val?: boolean): Query<T>;
+    elemMatch(path: string, val: any): Query<T>;
+    elemMatch(val: any): Query<T>;
+    intersects: Query<T>;
+    select(arg: Object): Query<T>;
+    select(arg: string): Query<T>;
+    slice(path: string, val: number): Query<T>;
+    slice(...val: number[]): Query<T>;
+    slice(val: number[]): Query<T>;
+    sort(arg: Object): Query<T>;
+    sort(arg: String): Query<T>;
+    limit(val: number): Query<T>;
+    skip(val: number): Query<T>;
+    maxscan(val: number): Query<T>;
+    batchSize(val: number): Query<T>;
+    comment(val: number): Query<T>;
+    snapShot(): Query<T>;
+    hint(val: Object): Query<T>;
+    slaveOk(v?: boolean): Query<T>;
+    read(pref: string, tags?: Object[]): Query<T>;
+    lean(v?: boolean): Query<T>;
+    tailable(v?: boolean): Query<T>;
+    findOne(callback: (err: any, result: any) => void): Query<T>;
+    count(callback: (err: any, result: number) => void): Query<T>;
+    distinct(field: string, callback: (err: any, result: any[]) => void): Query<T>;
+    update(doc: Object, callback?: (err: any, numberAffected?: number, raw?: any) => void): Query<T>;
+    remove(callback?: (err: any) => void): Query<T>;
+    findOneAndUpdate(query: Object, doc: Object, options: Object, callback?: (err: any, doc: any) => void): Query<T>;
+    findOneAndUpdate(query: Object, doc: Object, callback?: (err: any, doc: any) => void): Query<T>;
+    findOneAndUpdate(callback?: (err: any, doc: any) => void): Query<T>;
+    findOneAndRemove(query: Object, options: Object, callback?: (err: any) => void): Query<T>;
+    findOneAndRemove(query: Object, callback?: (err: any) => void): Query<T>;
+    findOneAndRemove(callback?: (err: any) => void): Query<T>;
+    populate(path: string, select?: Object, model?: Model<Document>, match?: Object, options?: Object): Query<T>;
+    populate(path: string, select?: string, model?: Model<Document>, match?: Object, options?: Object): Query<T>;
+    populate(args: Object): Query<T>;
 
-    skip(x: number): Query<any>;
-    limit(x: number): Query<any>;
+    stream(): QueryStream;
+    within: QueryWithin<T>;
   }
 
-  class Promise {
+  interface QueryWithin<T extends Document> extends Query<T> {
+    box(path: string, val: Object): QueryWithin<T>;
+    box(val: Object): QueryWithin<T>;
+    center(path: string, val: Object, opts?: Object): QueryWithin<T>;
+    center(val: Object, opts?: Object): QueryWithin<T>;
+    centerSphere(path: string, val: Object): QueryWithin<T>;
+    centerSphere(val: Object): QueryWithin<T>;
+    polygon(path: string, val: Array): QueryWithin<T>;
+    polygon(val: Array): QueryWithin<T>;
+    polygon(path: string, val: Object): QueryWithin<T>;
+    polygon(val: Object): QueryWithin<T>;
+    geometry(path: string, val: Object): QueryWithin<T>;
+    geometry(val: Object): QueryWithin<T>;
+  }
+
+  interface Promise {
+    on(event: string, listener: Function): Promise;
+    reject(reason: any): Promise;
+    error(err: any): Promise;
+    resolve(err?: any, val?: Object): void;
+    addBack(listener: Function): Promise;
+    complete(...args: any[]): void;
+    addCallback(listener: Function): Promise;
+    addErrback(listener: Function): Promise;
+    then(onFulfill: Function, onReject?: Function): Promise;
+    end(): void;
   }
 
   interface Model<T extends Document> {
@@ -231,9 +449,6 @@ declare module "mongoose" {
     }
   }
 
-  class Types {
-  }
-
-  class SchemaTypes {
-  }
+  var Types: MongooseTypes;
+  var SchemaTypes: SchemaTypes;
 }
